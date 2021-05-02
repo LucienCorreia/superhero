2f1555c6e816659afe3d9272852ec16f117b5335
Lucien <lucienrc@outlook.com.br>
2021-05-01 21:51:11 -0300

    chore: create function for generate heroes for tests

--- test/domain/use_cases/get_heroes_use_case_test.dart
+++ test/domain/use_cases/get_heroes_use_case_test.dart
@@ -14,6 +14,8 @@ import 'package:superhero/domain/entities/work_entity.dart';
 import 'package:superhero/domain/repositories/hero_repository.dart';
 import 'package:superhero/domain/use_cases/get_heroes_use_case.dart';
 
+import '../../utils/generate_heroes.dart';
+
 class HeroRepositoryMock implements HeroRepositoryInterface {
   final List<HeroEntity> heroes;
 
@@ -32,75 +34,7 @@ void main() {
     List<HeroEntity> heroes = [];
 
     setUpAll(() async {
-      final json = await rootBundle.loadString('test/assets/jsons/heroes.json');
-
-      List<dynamic> jsonHeroes = jsonDecode(json);
-
-      jsonHeroes.forEach((value) {
-        final powerStats = value['powerstats'];
-        final images = value['images'];
-        final appearance = value['appearance'];
-        final height = appearance['height'];
-        final weight = appearance['weight'];
-        final biography = value['biography'];
-        final work = value['work'];
-        final connections = value['connections'];
-
-        heroes.add(
-          HeroEntity(
-            id: value['id'],
-            name: value['name'],
-            slug: value['slug'],
-            powerStats: PowerStatsEntity(
-              intelligence: powerStats['intelligence'],
-              strength: powerStats['strength'],
-              speed: powerStats['speed'],
-              durability: powerStats['durability'],
-              power: powerStats['power'],
-              combat: powerStats['combat'],
-            ),
-            appearance: AppearanceEntity(
-              gender: appearance['gender'],
-              race: appearance['race'],
-              eyeColor: appearance['eyeColor'],
-              hairColor: appearance['hairColor'],
-              height: HeightEntity(
-                inch: height[0],
-                centimeters: height[1],
-              ),
-              weight: WeightEntity(
-                pound: weight[0],
-                kilograms: weight[1],
-              ),
-            ),
-            biography: BiographyEntity(
-              fullName: biography['fullName'],
-              alterEgos: biography['alterEgos'],
-              aliases: (biography['aliases'] as Iterable)
-                  .map((e) => String.fromEnvironment(e ?? ''))
-                  .toList(),
-              placeOfBirth: biography['placeOfBirth'],
-              firstAppearance: biography['firstAppearance'],
-              publisher: biography['publisher'],
-              alignment: biography['alignment'],
-            ),
-            work: WorkEntity(
-              occupation: work['occupation'],
-              base: work['base'],
-            ),
-            connections: ConnectionsEntity(
-              groupAffiliation: connections['groupAffiliation'],
-              relatives: connections['relatives'],
-            ),
-            images: ImagesEntity(
-              xs: images['xs'],
-              sm: images['sm'],
-              md: images['md'],
-              lg: images['lg'],
-            ),
-          ),
-        );
-      });
+      heroes = await generateHeroes();
     });
 
     test('should all heroes', () async {
